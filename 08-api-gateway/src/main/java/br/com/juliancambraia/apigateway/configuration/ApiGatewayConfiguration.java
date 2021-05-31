0dcb17595388dd1fb6a45a0a0469e67b5e225bbc
JulianCambraia <juliancambraia@gmail.com>
2021-05-30 22:38:43 -0300

    Adiciona novas rotas para services com load balance

--- 08-api-gateway/src/main/java/br/com/juliancambraia/apigateway/configuration/ApiGatewayConfiguration.java
+++ 08-api-gateway/src/main/java/br/com/juliancambraia/apigateway/configuration/ApiGatewayConfiguration.java
@@ -1,14 +1,9 @@
 package br.com.juliancambraia.apigateway.configuration;
 
-import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.Buildable;
-import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.function.Function;
 
 @Configuration
 public class ApiGatewayConfiguration {
@@ -16,15 +11,16 @@ public class ApiGatewayConfiguration {
     @Bean
     public RouteLocator getewayRouterLocator(RouteLocatorBuilder builder) {
 
-        Function<PredicateSpec, Buildable<Route>> minhaFunction =
-                p -> p.path("/get")
+        return builder.routes()
+                .route(p -> p.path("/get")
                         .filters(f -> f
                                 .addRequestHeader("Hello", "World")
                                 .addRequestParameter("paramHello", "World"))
-                        .uri("http://httpbin.org:80");
-
-        return builder.routes()
-                .route(minhaFunction)
+                        .uri("http://httpbin.org:80"))
+                .route(p -> p.path("/cambio-service/**")
+                        .uri("lb://cambio-service"))
+                .route(p -> p.path("/book-service/**")
+                        .uri("lb://book-service"))
                 .build();
     }
 }
