0f81fdc4138f2fde47a4e23e9a8f9b039c44567c
JulianCambraia <juliancambraia@gmail.com>
2021-06-04 00:04:50 -0300

    Add OpenAPI Doc and Configuration.

--- 06-book-service/src/main/java/br/com/juliancambraia/bookservice/controller/BookController.java
+++ 06-book-service/src/main/java/br/com/juliancambraia/bookservice/controller/BookController.java
@@ -1,14 +1,17 @@
 package br.com.juliancambraia.bookservice.controller;
 
 import br.com.juliancambraia.bookservice.model.Book;
-import br.com.juliancambraia.bookservice.repository.BookRepository;
 import br.com.juliancambraia.bookservice.proxy.CambioProxy;
+import br.com.juliancambraia.bookservice.repository.BookRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Book endpoint")
 @RestController
 @RequestMapping("book-service")
 public class BookController {
@@ -25,6 +28,7 @@ public class BookController {
         this.proxy = proxy;
     }
 
+    @Operation(summary = "Find a specific book by your ID")
     @GetMapping(value = "/{id}/{currency}")
     public Book findBook(@PathVariable("id") Long id, @PathVariable("currency") String currency) {
         var book = repository.getById(id);
--- 06-book-service/src/main/java/br/com/juliancambraia/bookservice/resilience4j/FooBarController.java
+++ 06-book-service/src/main/java/br/com/juliancambraia/bookservice/resilience4j/FooBarController.java
@@ -4,6 +4,8 @@ import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +13,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@Tag(name = "Foo bar")
 @RestController
 @RequestMapping("/book-service")
 public class FooBarController {
 
     private Logger logger = LoggerFactory.getLogger(FooBarController.class);
 
+    @Operation(summary = "Foobar Retry")
     @GetMapping("/foo-bar")
     @Retry(name = "foo-bar-retry", fallbackMethod = "responseErrorFallback")
     public String fooBar() {
@@ -28,6 +32,7 @@ public class FooBarController {
         return "FallbackMethod Foo-bar.!!!";
     }
 
+    @Operation(summary = "Foobar Circuitbreaker")
     @GetMapping("/foo-bar-circuitbreaker")
     @CircuitBreaker(name = "foo-bar-circuit-breaker", fallbackMethod = "responseErrorFallbackCircuitBreaker")
     public String fooBarCircuitBreaker() {
@@ -39,6 +44,7 @@ public class FooBarController {
         return "Request to Foo-bar-circuit-breaker";
     }
 
+    @Operation(summary = "Foobar Ratelimiter")
     @GetMapping("/foo-bar-ratelimiter")
     @RateLimiter(name = "foo-bar-rate-limiter")
     public String fooBarRateLimiter() {
@@ -47,6 +53,7 @@ public class FooBarController {
         return "Foo-bar-rate-limiter";
     }
 
+    @Operation(summary = "Foobar Bulkhead")
     @GetMapping("/foo-bar-bulkhead")
     @Bulkhead(name = "foo-bar-bulkhead")
     public String fooBarBulkHead() {
